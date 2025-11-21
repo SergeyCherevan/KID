1d542322bcd57ba3b14b3c04a5c81c56f4d39832
Сергей Черевань <CherevanSergeyKharkov@gmail.com>
2025-11-22 00:36:24 +0100

    fix error with brush

--- KID/KIDLibrary/Graphics.Color.cs
+++ KID/KIDLibrary/Graphics.Color.cs
@@ -5,47 +5,88 @@ namespace KID
     // Вспомогательная структура для работы с цветами в разных форматах
     public struct ColorValue
     {
+        private enum ColorType
+        {
+            String,
+            Rgb,
+            Int,
+            Brush
+        }
+
+        private ColorType type;
+        private string colorName;
+        private (byte r, byte g, byte b)? rgb;
+        private int? rgbInt;
         private Brush brush;
 
-        private ColorValue(Brush brush)
+        private ColorValue(ColorType type, string colorName = null, (byte r, byte g, byte b)? rgb = null, int? rgbInt = null, Brush brush = null)
         {
-            this.brush = brush ?? Brushes.Black;
+            this.type = type;
+            this.colorName = colorName;
+            this.rgb = rgb;
+            this.rgbInt = rgbInt;
+            this.brush = brush;
         }
 
         public static implicit operator ColorValue(string colorName)
         {
-            try
-            {
-                var converter = new BrushConverter();
-                return new ColorValue((Brush)converter.ConvertFromString(colorName));
-            }
-            catch
-            {
-                return new ColorValue(Brushes.Black);
-            }
+            return new ColorValue(ColorType.String, colorName: colorName);
         }
 
         public static implicit operator ColorValue((byte r, byte g, byte b) rgb)
         {
-            return new ColorValue(new SolidColorBrush(Color.FromRgb(rgb.r, rgb.g, rgb.b)));
+            return new ColorValue(ColorType.Rgb, rgb: rgb);
         }
 
         public static implicit operator ColorValue(int rgb)
         {
-            byte red = (byte)((rgb >> 16) & 0xFF);
-            byte green = (byte)((rgb >> 8) & 0xFF);
-            byte blue = (byte)(rgb & 0xFF);
-            return new ColorValue(new SolidColorBrush(Color.FromRgb(red, green, blue)));
+            return new ColorValue(ColorType.Int, rgbInt: rgb);
         }
 
         public static implicit operator ColorValue(Brush brush)
         {
-            return new ColorValue(brush);
+            return new ColorValue(ColorType.Brush, brush: brush);
         }
 
-        public static implicit operator Brush(ColorValue colorValue)
+        // Создаёт Brush в UI потоке
+        internal Brush CreateBrush()
         {
-            return colorValue.brush;
+            switch (type)
+            {
+                case ColorType.String:
+                    try
+                    {
+                        var converter = new BrushConverter();
+                        return (Brush)converter.ConvertFromString(colorName) ?? Brushes.Black;
+                    }
+                    catch
+                    {
+                        return Brushes.Black;
+                    }
+
+                case ColorType.Rgb:
+                    if (rgb.HasValue)
+                    {
+                        return new SolidColorBrush(Color.FromRgb(rgb.Value.r, rgb.Value.g, rgb.Value.b));
+                    }
+                    return Brushes.Black;
+
+                case ColorType.Int:
+                    if (rgbInt.HasValue)
+                    {
+                        byte red = (byte)((rgbInt.Value >> 16) & 0xFF);
+                        byte green = (byte)((rgbInt.Value >> 8) & 0xFF);
+                        byte blue = (byte)(rgbInt.Value & 0xFF);
+                        return new SolidColorBrush(Color.FromRgb(red, green, blue));
+                    }
+                    return Brushes.Black;
+
+                case ColorType.Brush:
+                    return brush ?? Brushes.Black;
+
+                default:
+                    return Brushes.Black;
+            }
         }
     }
 
@@ -58,7 +99,7 @@ namespace KID
             {
                 InvokeOnUI(() =>
                 {
-                    fillBrush = value;
+                    fillBrush = value.CreateBrush();
                 });
             }
         }
@@ -70,7 +111,7 @@ namespace KID
             {
                 InvokeOnUI(() =>
                 {
-                    strokeBrush = value;
+                    strokeBrush = value.CreateBrush();
                 });
             }
         }
@@ -82,8 +123,9 @@ namespace KID
             {
                 InvokeOnUI(() =>
                 {
-                    fillBrush = value;
-                    strokeBrush = value;
+                    var brush = value.CreateBrush();
+                    fillBrush = brush;
+                    strokeBrush = brush;
                 });
             }
         }
