96428d4858c1849bc5193a09b58944185e32e1ec
Сергей Черевань <CherevanSergeyKharkov@gmail.com>
2025-11-23 19:34:22 +0100

    refactoring: `Test of Stop button.cs`

--- KID/ProjectTemplates/Test of Stop button.cs
+++ KID/ProjectTemplates/Test of Stop button.cs
@@ -8,13 +8,13 @@ for (int i = 0; i < 1000000; i++)
 
     Console.WriteLine($"Обработка {i}");
     
-    KID.Graphics.Color = (
+    Graphics.Color = (
         (byte)((Math.Sin(2*Math.PI * i/0x100 / 3 + 0 * 2*Math.PI/3) + 1) * 0xFF/2),
         (byte)((Math.Sin(2*Math.PI * i/0x100 / 3 + 1 * 2*Math.PI/3) + 1) * 0xFF/2),
         (byte)((Math.Sin(2*Math.PI * i/0x100 / 3 + 2 * 2*Math.PI/3) + 1) * 0xFF/2)
     );
     
-	KID.Graphics.Circle(
+	Graphics.Circle(
         150 + Math.Sin(2*Math.PI / 3 * i/0x100) * 100,
         150 + Math.Cos(2*Math.PI / 3 * i/0x100) * 100,
         10
