705bff1e70739166a013ef44a8ed51504bc22e5b
Сергей Черевань <CherevanSergeyKharkov@gmail.com>
2025-11-23 23:24:30 +0100

    add `HelloWorld. Full structure.cs`

---
+++ KID/ProjectTemplates/HelloWorld. Full structure.cs
@@ -0,0 +1,20 @@
+namespace KIDProject
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			System.Console.WriteLine("Hello World!");
+			
+			KID.Graphics.Color = (255, 0, 0);
+			KID.Graphics.Circle(150, 150, 125);
+			
+			KID.Graphics.Color = 0x0000FF;
+			KID.Graphics.Rectangle(150, 150, 100, 100);
+			
+			KID.Graphics.Color = "White";
+			KID.Graphics.SetFont("Arial", 25);
+			KID.Graphics.Text(150, 150, "Hello\nWorld!");
+		}
+	}
+}
