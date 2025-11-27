ce021a0075fc31ee74b1c2db372848bf19e95717
Сергей Черевань <CherevanSergeyKharkov@gmail.com>
2025-11-27 15:04:24 +0100

    refactoring ColorValue

--- KID/KIDLibrary/Graphics/Graphics.Color.cs
+++ KID/KIDLibrary/Graphics/Graphics.Color.cs
@@ -1,92 +1,59 @@
-﻿using System.Windows.Media;
+﻿using System;
+using System.Windows.Media;
 
 namespace KID
 {
     // Вспомогательная структура для работы с цветами в разных форматах
     public struct ColorValue
     {
-        private enum ColorType
-        {
-            String,
-            Rgb,
-            Int,
-            Brush
-        }
+        private readonly Func<Brush> _brushFactory;
 
-        private ColorType type;
-        private string colorName;
-        private (byte r, byte g, byte b)? rgb;
-        private int? rgbInt;
-        private Brush brush;
-
-        private ColorValue(ColorType type, string colorName = null, (byte r, byte g, byte b)? rgb = null, int? rgbInt = null, Brush brush = null)
+        internal ColorValue(Func<Brush> brushFactory)
         {
-            this.type = type;
-            this.colorName = colorName;
-            this.rgb = rgb;
-            this.rgbInt = rgbInt;
-            this.brush = brush;
+            _brushFactory = brushFactory ?? (() => Brushes.Black);
         }
 
         public static implicit operator ColorValue(string colorName)
         {
-            return new ColorValue(ColorType.String, colorName: colorName);
+            return new ColorValue(() =>
+            {
+                try
+                {
+                    var converter = new BrushConverter();
+                    return converter.ConvertFromString(colorName) as Brush ?? Brushes.Black;
+                }
+                catch
+                {
+                    return Brushes.Black;
+                }
+            });
         }
 
         public static implicit operator ColorValue((byte r, byte g, byte b) rgb)
         {
-            return new ColorValue(ColorType.Rgb, rgb: rgb);
+            return new ColorValue(() => new SolidColorBrush(Color.FromRgb(rgb.r, rgb.g, rgb.b)));
         }
 
         public static implicit operator ColorValue(int rgb)
         {
-            return new ColorValue(ColorType.Int, rgbInt: rgb);
+            return new ColorValue(() =>
+            {
+                byte red = (byte)((rgb >> 16) & 0xFF);
+                byte green = (byte)((rgb >> 8) & 0xFF);
+                byte blue = (byte)(rgb & 0xFF);
+                return new SolidColorBrush(Color.FromRgb(red, green, blue));
+            });
         }
 
         public static implicit operator ColorValue(Brush brush)
         {
-            return new ColorValue(ColorType.Brush, brush: brush);
+            return new ColorValue(() => brush ?? Brushes.Black);
         }
 
         // Создаёт Brush в UI потоке
         internal Brush CreateBrush()
         {
-            switch (type)
-            {
-                case ColorType.String:
-                    try
-                    {
-                        var converter = new BrushConverter();
-                        return (Brush)converter.ConvertFromString(colorName) ?? Brushes.Black;
-                    }
-                    catch
-                    {
-                        return Brushes.Black;
-                    }
-
-                case ColorType.Rgb:
-                    if (rgb.HasValue)
-                    {
-                        return new SolidColorBrush(Color.FromRgb(rgb.Value.r, rgb.Value.g, rgb.Value.b));
-                    }
-                    return Brushes.Black;
-
-                case ColorType.Int:
-                    if (rgbInt.HasValue)
-                    {
-                        byte red = (byte)((rgbInt.Value >> 16) & 0xFF);
-                        byte green = (byte)((rgbInt.Value >> 8) & 0xFF);
-                        byte blue = (byte)(rgbInt.Value & 0xFF);
-                        return new SolidColorBrush(Color.FromRgb(red, green, blue));
-                    }
-                    return Brushes.Black;
-
-                case ColorType.Brush:
-                    return brush ?? Brushes.Black;
-
-                default:
-                    return Brushes.Black;
-            }
+            return _brushFactory?.Invoke() ?? Brushes.Black;
         }
     }
 
@@ -94,7 +61,7 @@ namespace KID
     {
         public static ColorValue FillColor
         {
-            get => fillBrush;
+            get => new ColorValue(() => fillBrush);
             set
             {
                 InvokeOnUI(() =>
@@ -106,7 +73,7 @@ namespace KID
 
         public static ColorValue StrokeColor
         {
-            get => strokeBrush;
+            get => new ColorValue(() => strokeBrush);
             set
             {
                 InvokeOnUI(() =>
@@ -118,7 +85,7 @@ namespace KID
 
         public static ColorValue Color
         {
-            get => fillBrush;
+            get => new ColorValue(() => fillBrush);
             set
             {
                 InvokeOnUI(() =>
