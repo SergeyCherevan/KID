5de53b0d97ef5c61ec2c038eb57f5beb7083c1bf
Сергей Черевань <CherevanSergeyKharkov@gmail.com>
2025-12-11 02:41:20 +0100

    refactor: enhance Graphics API - return shape elements, add extension methods and Bezier curves

---
+++ KID/KIDLibrary/Graphics/Graphics.ShapeMethodes.cs
@@ -0,0 +1,158 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace KID
+{
+    public static partial class Graphics
+    {
+        public static Shape? SetLeftX(this Shape element, double x)
+        {
+            return InvokeOnUI(() =>
+            {
+                if (Canvas == null) return null;
+                Canvas.SetLeft(element, x);
+                return element;
+            });
+        }
+        public static Shape? SetTopY(this Shape element, double y)
+        {
+            return InvokeOnUI(() =>
+            {
+                if (Canvas == null) return null;
+                Canvas.SetTop(element, y);
+                return element;
+            });
+        }
+        public static Shape? SetLeftTopXY(this Shape element, double x, double y)
+        {
+            return InvokeOnUI(() =>
+            {
+                if (Canvas == null) return null;
+                Canvas.SetLeft(element, x);
+                Canvas.SetTop(element, y);
+                return element;
+            });
+        }
+        public static Shape? SetLeftTopXY(this Shape element, Point point)
+        {
+            return SetLeftTopXY(element, point.X, point.Y);
+        }
+
+        public static Shape? SetCenterX(this Shape element, double x)
+        {
+            return InvokeOnUI(() =>
+            {
+                if (Canvas == null) return null;
+                Canvas.SetLeft(element, x - element.ActualHeight / 2);
+                return element;
+            });
+        }
+        public static Shape? SetCenterY(this Shape element, double y)
+        {
+            return InvokeOnUI(() =>
+            {
+                if (Canvas == null) return null;
+                Canvas.SetTop(element, y - element.ActualHeight / 2);
+                return element;
+            });
+        }
+        public static Shape? SetCenterXY(this Shape element, double x, double y)
+        {
+            return InvokeOnUI(() =>
+            {
+                if (Canvas == null) return null;
+                Canvas.SetLeft(element, x - element.ActualWidth / 2);
+                Canvas.SetTop(element, y - element.ActualHeight / 2);
+                return element;
+            });
+        }
+        public static Shape? SetCenterXY(this Shape element, Point point)
+        {
+            return SetCenterXY(element, point.X, point.Y);
+        }
+
+        public static Shape? SetWidth(this Shape element, double width)
+        {
+            return InvokeOnUI(() =>
+            {
+                if (Canvas == null) return null;
+                element.Width = width;
+                return element;
+            });
+        }
+        public static Shape? SetHeight(this Shape element, double height)
+        {
+            return InvokeOnUI(() =>
+            {
+                if (Canvas == null) return null;
+                element.Height = height;
+                return element;
+            });
+        }
+        public static Shape? SetSize(this Shape element, double width, double height)
+        {
+            return InvokeOnUI(() =>
+            {
+                if (Canvas == null) return null;
+                element.Width = width;
+                element.Height = height;
+                return element;
+            });
+        }
+        public static Shape? SetSize(this Shape element, Point size)
+        {
+            return SetSize(element, size.X, size.Y);
+        }
+
+        public static Shape? SetStrokeColor(this Shape element, ColorType color)
+        {
+            return InvokeOnUI(() =>
+            {
+                if (Canvas == null) return null;
+                element.Stroke = color.CreateBrush();
+                return element;
+            });
+        }
+        public static Shape? SetFillColor(this Shape element, ColorType color)
+        {
+            return InvokeOnUI(() =>
+            {
+                if (Canvas == null) return null;
+                element.Fill = color.CreateBrush();
+                return element;
+            });
+        }
+        public static Shape? SetColor(this Shape element, ColorType color)
+        {
+            return InvokeOnUI(() =>
+            {
+                if (Canvas == null) return null;
+                element.Stroke = color.CreateBrush();
+                element.Fill = color.CreateBrush();
+                return element;
+            });
+        }
+        
+        public static Shape? AddToCanvas(this Shape element)
+        {
+            return InvokeOnUI(() =>
+            {
+                if (Canvas == null) return null;
+                Canvas.Children.Add(element);
+                return element;
+            });
+        }
+
+        public static Shape? RemoveFromCanvas(this Shape element)
+        {
+            return InvokeOnUI(() =>
+            {
+                if (Canvas == null) return null;
+                Canvas.Children.Remove(element);
+                return element;
+            });
+        }
+    }
+}
--- KID/KIDLibrary/Graphics/Graphics.System.cs
+++ KID/KIDLibrary/Graphics/Graphics.System.cs
@@ -1,9 +1,61 @@
-﻿using System.Windows.Media;
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace KID
 {
     public static partial class Graphics
     {
+        public static Canvas Canvas { get; private set; }
+
+        private static Brush fillBrush = Brushes.Black;
+        private static Brush strokeBrush = Brushes.Black;
+        private static Typeface currentFont = new Typeface("Arial");
+        private static double currentFontSize = 20;
+        private static Dispatcher dispatcher;
+
+        public static void Init(Canvas targetCanvas)
+        {
+            if (targetCanvas == null)
+                throw new ArgumentNullException(nameof(targetCanvas));
+            
+            Canvas = targetCanvas;
+            dispatcher = Application.Current?.Dispatcher;
+        }
+
+        public static void InvokeOnUI(Action action)
+        {
+            if (action == null)
+                return;
+            
+            if (dispatcher == null || dispatcher.CheckAccess())
+            {
+                action();
+            }
+            else
+            {
+                dispatcher.BeginInvoke(action, DispatcherPriority.Background);
+            }
+        }
+
+        public static T InvokeOnUI<T>(Func<T> func)
+        {
+            if (func == null)
+                return default(T)!;
+            
+            if (dispatcher == null || dispatcher.CheckAccess())
+            {
+                return func();
+            }
+            else
+            {
+                T result = default(T)!;
+                dispatcher.Invoke(() => { result = func(); }, DispatcherPriority.Background);
+                return result;
+            }
+        }
+
         public static void Clear()
         {
             InvokeOnUI(() =>
